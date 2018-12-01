7a59588f80c02a04af9f2cdeb2c44a8782b0b596
Artsiom <artkostm@gmail.com>
2018-12-01 13:44:14 +0300

    move case classes to package object

--- src/main/scala/com/artkostm/glossary/package.scala
+++ src/main/scala/com/artkostm/glossary/package.scala
@@ -1,5 +1,18 @@
 package com.artkostm
 
-package object glossary {
+import akka.http.scaladsl.model.StatusCode
 
+package object glossary {
+  case class Translation(text: String)
+
+  case class Definition(text: String, pos: Option[String], ts: Option[String], fl: Option[String], tr: List[Translation])
+
+  case class DictResp(`def`: List[Definition])
+
+
+  sealed trait ApiError
+
+  case class NotFound(error: String) extends ApiError
+
+  case class UnexpectedStatusCode(status: StatusCode) extends ApiError
 }
